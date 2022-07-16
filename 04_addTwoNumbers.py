736b8a84fcaec37fad309df7cee1d98806ae9a41
shanny12 <kudreppadysharanya@gmail.com>
2022-07-16 10:55:42 +0530

    feat: adding two numbers

---
+++ 04_addTwoNumbers.py
@@ -0,0 +1,12 @@
+'''
+Author : Sharanya D
+Date   : 16/07/2022
+
+Add two Numbers :
+Take the input as two numbers and add that and display the result
+'''
+
+num1=int(input('enter the 1st number: '))
+num2=int(input('Enter the 2nd number: '))
+sum=num1+num2
+print('the sum of {0} and {1} is {2}'.format(num1,num2,sum) )
